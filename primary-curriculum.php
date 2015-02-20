ac3fedc74c3e1e20435877e0358d69ed4e3f8608
jon <jonabc7@gmail.com>
2015-02-20 15:00:23 -0500

    layout of primary curriculun

--- primary-curriculum.php
+++ primary-curriculum.php
@@ -1,6 +1,67 @@
 <?php require_once "twig_instance.php";
 require 'layout.php';
 require 'sidebar-primary.php';
-$page=array('' => '' );
+$page=array(
+	'metaTitle'=>'',
+    'metaDescription'=>'',
+    'title' => 'CURRICULUM',
+    'curriculums' => array(
+    	'0' =>  array(
+    		'title' => '1st Grade', 
+    		'items' => array(
+    			'0' => array('name' => 'Spanish','link' => 'static/pdf/1esp.pdf' ),
+    			'1' => array('name' => 'Spanish','link' => 'static/pdf/1esp.pdf' ),
+    			'2' => array('name' => 'Spanish','link' => 'static/pdf/1esp.pdf' ),
+    			'3' => array('name' => 'Spanish','link' => 'static/pdf/1esp.pdf' ),
+    			)
+    	),
+    	'1' =>  array(
+    		'title' => '2nd Grade', 
+    		'items' => array(
+    			'0' => '',
+    			'1' => '',
+    			'2' => '',
+    			'3' => '',
+    			)
+    	),
+    	'2' =>  array(
+    		'title' => '3rd Grade', 
+    		'items' => array(
+    			'0' => '',
+    			'1' => '',
+    			'2' => '',
+    			'3' => '',
+    			)
+    	),
+    	'3' =>  array(
+    		'title' => '4th Grade', 
+    		'items' => array(
+    			'0' => '',
+    			'1' => '',
+    			'2' => '',
+    			'3' => '',
+    			)
+    	),
+    	'4' =>  array(
+    		'title' => '5th Grade', 
+    		'items' => array(
+    			'0' => '',
+    			'1' => '',
+    			'2' => '',
+    			'3' => '',
+    			)
+    	),
+    	'5' =>  array(
+    		'title' => '6th Grade', 
+    		'items' => array(
+    			'0' => '',
+    			'1' => '',
+    			'2' => '',
+    			'3' => '',
+    			)
+    	)
+
+    )
+ );
 echo $twig->render('primary-curriculum.html',array('main_menu'=>$main_menu,'footer'=>$footer,'sidebar'=>$sidebar,'page'=>$page));
 ?>
