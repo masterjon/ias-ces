8445b284baa5ff1d8d59b6c06d68d25e41617726
Enrique <enrique@punkmkt.com>
2015-02-23 14:05:03 -0500

    agregado contenido de curriculum primary, high school testing y description

--- middle-high-school-testing.php
+++ middle-high-school-testing.php
@@ -2,6 +2,110 @@
 <?php require_once "twig_instance.php";
 require 'layout.php';
 require 'sidebar-middle-high-school.php';
-$page=array('' => '' );
+$page_en=array(
+  'metaTitle'=>'',
+  'metaDescription'=>'',
+  'title' => 'Testing',
+  'subtitle' => '',
+  'content'=>'<p>The following is a suggested schedule of testing for students planning to apply to universities in Mexico AND outside Mexico.</p>
+  					<h4>JUNIOR YEAR (11th Grade):</h4>
+  					<ul class="margin-15">
+  						<li>October: PSAT/NMSQT (All IAS students in grades 9-11 take this test!)</li>
+  						<li>April: ACT with Writing</li>
+  						<li>May: SAT Reasoning Test</li>
+  						<li>June: SAT Subject Tests (If you are planning on attending a university that requires these tests.)</li>
+  					</ul>
+  					<h4>SENIOR YEAR (12th Grade):</h4>
+  					<ul class="margin-15">
+  						<li>September/October: PAA for Mexican universities</li>
+  						<li>October/November: Retake ACT, SAT, and/or SAT Subject Tests*</li>
+  						<li>December/January: Last chance for retaking SAT and/or SAT Subject Tests**</li>
+  					</ul>
+  					<p>You may also need to take the TOEFL</p>
+  					<p>*Only if you are not satisfied with your original scores.</p>
+  					<p>**Only if you are not satisfied with your scores to date. Also, be sure to check whether these late testing dates will be accepted by the universities to which you are applying.</p>
+  					<h4>Resources for the PSAT, PAA, SAT, ACT, and TOEFL</h4>
+  					<h4>PSAT/NMSQT</h4>
+  					<p>The PSAT/NMSQT is given to all IAS students in grades 9-11 in October of each year. http://www.collegeboard.com/student/testing/psat/about.html</p>
+  					<h4>PAA</h4>
+  					<p>The PAA (Prueba de Aptitud Acad�mica) was developed by the College Board for Puerto Rico as an equivalent to the SAT, but in Spanish. It is the admissions test used by many universities in Latin America (UDLA, ITESM, UDEM, Anahuac, etc.) The ITESM of Puebla (TEC) administers the test to all IAS juniors in the spring as a diagnostic and practice tool and to seniors in October. The UDEM and UDLA offer the test several times during the school year at different places in Cancun.</p>
+  					<p>Although many universities use the PAA, it is recommended that you take it separately for the each school. The TEC will not accept UDEMs test. UDEM will accept the TECs test, but you will have to pay to have your results sent to UDEM.</p>
+  					<p>Both the TEC and UDEM will revalidate your SAT scores, if they are high enough. <a targe="_blank" href="http://oprla.collegeboard.com/ptorico/pr/program/paa.html">http://oprla.collegeboard.com/ptorico/pr/program/paa.html</a></p>
+  					<h4>SAT and ACT</h4>
+  					<p>The International American School of Cancun is an official test center for the SAT and ACT, the two tests required by most US and some Canadian colleges and universities. Students must register for the test and pay the test fees online. Once you are registered, you can register for a test, check your scores, or have the scores sent to universities. When you register for a test, you will need to enter your current high school and the test center at which you want to test. If you are a student at IAS, our high school code is 870070. Our test center number is 69137. Test dates for the 2011-2012 school year are given below. You must register by the deadline to avoid paying a much higher fee for standby administration (only available on a space available basis).</p>
+  					<p>The SAT Reasoning Test has three sections: Critical Reading, Math, and Writing.</p>
+  					<p>Students who are planning to apply to universities outside Mexico are encouraged to take the SAT at least once before their senior year (May or June at the end of 11th grade). The SAT has a penalty for guessing, so it is important to practice before you take the test.</p>
+  					<p>SAT Subject Tests: The first thing you should do is to find out if you need to take these tests. Not all universities ask for SAT Subject Tests. Find out if the universities you are considering ask for them. If you are planning to apply to any highly selective universities, subject tests are probably a good idea, but you will still need to do your research to find out if there are specific tests you should take. <a targe="_balnk" href="www.collegeboard.org">www.collegeboard.org</a></p>
+  					<p>The ACT has four sections: English, Math, Reading, and Science. There is a fifth section for writing which is offered every testing date except February. Some universities will accept the ACT with Writing instead of the SAT and SAT subject tests! The ACT is a great test for students who can work quickly. There is no penalty for guessing but there are many questions for the amount of time, so you have to work quickly.<a target="_balnk" href="www.act.org">www.act.or</a></p>
+  					<h4>Test Dates and Deadlines for SAT and ACT 2013-14</h4>
+  					<table class="table-bordered">
+					  <tr>
+					    <th colspan="2">SAT</th>
+					    <th colspan="2">ACT</th>
+					  </tr>
+					  <tr>
+					    <td>Test Dates</td>
+					    <td>RegistrationDeadline</td>
+					    <td>Test Dates</td>
+					    <td>RegistrationDeadline</td>
+					  </tr>
+					  <tr>
+					    <td>Oct 11</td>
+					    <td>Sept 12</td>
+					    <td></td>
+					    <td></td>
+					  </tr>
+					  <tr>
+					    <td>Nov 8</td>
+					    <td>Oct 9</td>
+					    <td>Oct 26</td>
+					    <td>Sept19</td>
+					  </tr>
+					  <tr>
+					    <td>Dec 6</td>
+					    <td>Nov 6</td>
+					    <td>Dec 13</td>
+					    <td>Noc 7</td>
+					  </tr>
+					  <tr>
+					    <td>Jan 24</td>
+					    <td>Dec 29</td>
+					    <td></td>
+					    <td></td>
+					  </tr>
+					  <tr>
+					    <td>May 2</td>
+					    <td>Apr 6</td>
+					    <td>Apr 18</td>
+					    <td>Mar 13</td>
+					  </tr>
+					  <tr>
+					    <td>Jun 6</td>
+					    <td>May 9</td>
+					    <td>Jun 13</td>
+					    <td></td>
+					  </tr>
+					</table>
+					<h4>TOEFL </h4>
+					<p>You may need to take the TOEFL, Test of English as a Foreign Language, if English is not the primary language in your home and you want to attend a university in an English-speaking country. There are four parts to the test: Speaking, Listening, Reading, and Writing.</p>
+					<p>You may be able to get a waiver by explaining to the university that your high school coursework has been in English. It is worth a try! Call the admissions office and ask to speak with someone about a TOEFL waiver. Introduce yourself and explain your situation, that you have been a student at the International American School of Cancun for X years and that the language of instruction is English. For all these years, your classes have been in English (except for Spanish class) and that when you graduate, you will receive a Mexican diploma and a United States High School Diploma. Is there anyway that the TOEFL can be waived? They may ask for more information (SAT Critical Reading scores, ACT scores).</p>
+					<p>If you need to take the test, you register and pay the test fees online. The closest testing centers are the Colegio Puerto Aventuras in Puerto AventurasPlaya del Carmen, and the Instituto Benjamin Franklin in Merida. More information can be found at  <a targe="_blank" href="www.ets.org/toefl">www.ets.org/toefl</a>.</p>
+					<p>No matter where you are planning to apply for university, testing is recommended and usually required. It is your responsibility to be sure that you have all the tests required for your universities and to make sure that your test scores have been sent to the universities!</p>
+  					',
+  );
+
+$page_es=array(
+  'metaTitle'=>'',
+  'metaDescription'=>'',
+  'title' => 'Escuelas',
+  'subtitle' => 'Eventos',
+  );
+
+if (LANG=='ES'){
+    $page=$page_es; 
+}
+else{
+    $page=$page_en;
+}
 echo $twig->render('middle-high-school-testing.html',array('main_menu'=>$main_menu,'footer'=>$footer,'sidebar'=>$sidebar,'page'=>$page));
 ?>
--- primary-curriculum.php
+++ primary-curriculum.php
@@ -10,54 +10,53 @@ $page=array(
     		'title' => '1st Grade', 
     		'items' => array(
     			'0' => array('name' => 'Spanish','link' => 'static/pdf/1esp.pdf' ),
-    			'1' => array('name' => 'Spanish','link' => 'static/pdf/1esp.pdf' ),
-    			'2' => array('name' => 'Spanish','link' => 'static/pdf/1esp.pdf' ),
-    			'3' => array('name' => 'Spanish','link' => 'static/pdf/1esp.pdf' ),
+    			'1' => array('name' => 'Math','link' => 'static/pdf/1roMatemáticas.pdf' ),
+    			'2' => array('name' => 'Standars','link' => 'static/pdf/Standards1stgrade.pdf' ),
+                '3' => array('name' => 'Technology','link' => 'static/pdf/Technology1stGrade.pdf' ),
     			)
     	),
     	'1' =>  array(
     		'title' => '2nd Grade', 
     		'items' => array(
-    			'0' => '',
-    			'1' => '',
-    			'2' => '',
-    			'3' => '',
+    			'0' => array('name' => 'Spanish','link' => 'static/pdf/2doEsp.pdf' ),
+                '1' => array('name' => 'Math','link' => 'static/pdf/2doMatematicas.pdf' ),
+                '2' => array('name' => 'Standars','link' => 'static/pdf/Standards2ndgrade.pdf' ),
+                '3' => array('name' => 'Technology','link' => 'static/pdf/Technology2ndGrade.pdf' ),
     			)
     	),
     	'2' =>  array(
     		'title' => '3rd Grade', 
     		'items' => array(
-    			'0' => '',
-    			'1' => '',
-    			'2' => '',
-    			'3' => '',
+    			'0' => array('name' => 'Spanish','link' => 'static/pdf/3roEsp.pdf' ),
+                '1' => array('name' => 'Math','link' => 'static/pdf/3roMatematicas.pdf' ),
+                '2' => array('name' => 'Technology','link' => 'static/pdf/Technology3rdGrade.pdf' ),
     			)
     	),
     	'3' =>  array(
     		'title' => '4th Grade', 
     		'items' => array(
-    			'0' => '',
-    			'1' => '',
-    			'2' => '',
-    			'3' => '',
+    			'0' => array('name' => 'Spanish','link' => 'static/pdf/4toEsp.pdf' ),
+                '1' => array('name' => 'Math','link' => 'static/pdf/4toMatematicas.pdf' ),
+                '2' => array('name' => 'Standars','link' => 'static/pdf/Standards4thgrade.pdf' ),
+                '3' => array('name' => 'Technology','link' => 'static/pdf/Technology4thGrade.pdf' ),
     			)
     	),
     	'4' =>  array(
     		'title' => '5th Grade', 
     		'items' => array(
-    			'0' => '',
-    			'1' => '',
-    			'2' => '',
-    			'3' => '',
+    			'0' => array('name' => 'Spanish','link' => 'static/pdf/5toEsp.pdf' ),
+                '1' => array('name' => 'Math','link' => 'static/pdf/5toMatematicas.pdf' ),
+                '2' => array('name' => 'Standars','link' => 'static/pdf/Standards5thgrade.pdf' ),
+                '3' => array('name' => 'Technology','link' => 'static/pdf/Technology5thGrade.pdf' ),
     			)
     	),
     	'5' =>  array(
     		'title' => '6th Grade', 
     		'items' => array(
-    			'0' => '',
-    			'1' => '',
-    			'2' => '',
-    			'3' => '',
+    			'0' => array('name' => 'Spanish','link' => 'static/pdf/6toEsp.pdf' ),
+                '1' => array('name' => 'Math','link' => 'static/pdf/6toMatematicas.pdf' ),
+                '2' => array('name' => 'Standars','link' => 'static/pdf/Standards6thgrade.pdf' ),
+                '3' => array('name' => 'Technology','link' => 'static/pdf/Technology6thGrade.pdf' ),
     			)
     	)
 
