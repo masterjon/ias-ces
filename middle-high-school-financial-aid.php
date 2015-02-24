2aed03c0c51aadf8863ce5d3ba9466aec468a9d5
Enrique <enrique@punkmkt.com>
2015-02-24 09:11:46 -0500

    agregado curriculum primary; high college guidance

--- middle-high-school-financial-aid.php
+++ middle-high-school-financial-aid.php
@@ -1,6 +1,46 @@
 <?php require_once "twig_instance.php";
 require 'layout.php';
 require 'sidebar-middle-high-school.php';
-$page=array('' => '' );
+$page_en=array(
+  'metaTitle'=>'',
+  'metaDescription'=>'',
+  'title' => 'Paying for School',
+  'subtitle' => '',
+  'content'=>'<p>One of the most important aspects of planning for a university career is developing a budget well in advance. Have a very honest conversation with your parents about exactly how much they are willing to pay for your education. You need a specific number. Be aware that if you are not a US citizen or permanent resident, you cannot qualify for Federal Financial Aid in the US. However, anyone can qualify for institutional money. The college that you are applying to has its own funds. Fill out the paperwork, and they will tell you if youve qualified for institutional money.</p>
+  			  <p>Do not think of the number your parents come up with as the maximum tuition/ fees/books/room and board amount that you should look for when you re applying to schools. For example, your parents can only pay $30,000, and you want to apply to a school. Having only $14,000, doesn t rule out the possibility of going to that school! Financial aid is often available and you might be able to make it work. It depends entirely on the school.</p>
+  			  <p>Be honest when you are completing the paperwork. Hiding assets can work against you. Also, know that they aren t going to expect your family to contribute all their money to paying for college. Colleges know that your parents have to continue paying the bills, saving for retirement, saving for your little brother to go to college, etc.</p>
+  			  <h4>Scholarships and Financial Aid</h4>
+  			  <p>Many schools in the U.S. and in other countries provide scholarships and financial assistance for students, but the country of citizenship usually affects the amount of aid available. There are some schools in the U.S. that guarantee to meet financial need of foreign students, and there are some schools in other countries that offer full scholarships to select students</p>
+  			  <p>U.S. citizens apply to colleges and universities for need-based financial aid through FAFSA, the Free Application for Federal Student Aid. Although the application cannot be submitted to universities until January 1 of the last year of high school, you can begin the process a couple of months before by applying for a pin and filling out worksheets available on the website. The pin is necessary to submit the application electronically, and both you and your parent(s) need a pin. Some federal money is also available for U.S. citizens to use at Canadian universities</p>
+  			  <p>Most schools require additional documentation of financial need, usually including income tax records (from the U.S., Mexico, or another country) and statements of assets.</p>
+  			  <p><b>Useful Links:</b><br>http://www.profileonline.collegeboard.com The CSS/PROFILE is an online form, provided by the College Board. Many schools ask applicants to complete the CSS/PROFILE as a part of their financial aid application. Many of the member colleges, universities, graduate and professional schools, and scholarship programs use theinformation collected on PROFILE to help them award nonfederal student aid funds.</p>
+  			  <p>http://www.fafsa.ed.gov FAFSA on the web to apply for college financial aid after January 1st of the senior year. Do not be fooled by another site, fafsa.com - they are one of the sites that wants you to pay to </p>
+  			  <p>http://www.myroad.com - The amount of aid available at U.S. universities is summarized here and is available at each school s website.</p>
+  			  <p>http://www.cois.org The Council of International Schools (CIS) provides financial aid information for their member universities throughout the world.</p>
+  			  <p>http://www.fastweb.com This website is a free service that can help you find scholarships. Enter some information about yourself to create a profile, and the site matches you with scholarships that it thinks for which they think you might be qualified. It takes some work!</p>
+  			  <p>http://www.edupass.org This site provides information for international students who are thinking about pursuing an education in the United States. There is also some information for international students who are interested in studying in countries other than the US, but the main focus is on higher education in the US. This guide contains up-to-date information about everything an international student needs to know about studying and living in the United States. It covers every topic, from college admissions and financial aid to culture shock and clothing sizes.</p>
+  			  <p>http://www.finaid.org/ This is similar to the above site, but it is for American citizens and permanent resident</p>
+  			  <p>http://www.mappingyourfuture.org/paying/standardcalculator.htm If you are thinking about taking a student loan, use this tool to figure out how much your payments will be after college.</p>
+  			  <p>http://www.questbridge.org QuestBridge is a non-profit program that links bright, motivated low-income students with educational and scholarship opportunities at some of the best colleges in the US.</p>
+  			  <p>http://www.internationalscholarships.com International scholarship resource for students wishing to study abroad. At this site, you will find the most comprehensive listing of grants, scholarships, loan programs, and other information to assist college and university students in their pursuit to study ab</p>
+  			  <p>http://www.oas.org/rowe The Leo S. Rowe Fund is a student loan program of the Organization of American States that helps citizens from Latin America and the Caribbean OAS Member countries finance their studies or research in accredited universities across the United States. The Rowe Fund loans are given to students on condition that upon completion of his or her studies, the beneficiary must promise to repay the loan in full and return to his or her country in order to further intercultural exchange and socioeconomic development in the region.</p>
+  			  <p>Student Visas - You will need a student visa to study in a foreign country. You apply for your visa after you have received a letter of acceptance from a university. You can apply for a US student visa at the US Consulate in Merida. Information on the procedure to obtain an F-1 Student Visa is available here. You can apply for a Canadian visa through the Canadian Embassy in Mexico City. Information on the procedure to obtain a Study Permit is available here.</p>
+  			  <p></p>
+  			  ',
+  );
+
+$page_es=array(
+  'metaTitle'=>'',
+  'metaDescription'=>'',
+  'title' => 'Escuelas',
+  'subtitle' => 'Eventos',
+  );
+
+if (LANG=='ES'){
+    $page=$page_es; 
+}
+else{
+    $page=$page_en;
+}
 echo $twig->render('middle-high-school-financial-aid.html',array('main_menu'=>$main_menu,'footer'=>$footer,'sidebar'=>$sidebar,'page'=>$page));
 ?>
--- middle-high-school-scholarship.php
+++ middle-high-school-scholarship.php
@@ -1,6 +1,69 @@
 <?php require_once "twig_instance.php";
 require 'layout.php';
 require 'sidebar-middle-high-school.php';
-$page=array('' => '' );
+$page_en=array(
+  'metaTitle'=>'',
+  'metaDescription'=>'',
+  'title' => 'Scholarship',
+  'subtitle' => '',
+  'content'=>'<h4>Instituto Suizo de Gastronomía y Hotelería  (ISU)</h4>
+  			  <p>www.isu.edu.mx</p>
+  			  <ul class="margin-15">
+  			  	<li>Por cada generación  del último ciclo escolar   1  (una)  beca  del  50% al alumno con promedio mínimo 9.0 (nueve) acumulado en los 3 años de bachillerato; esta beca será valida para estudiar en el ISU cualquiera de sus licenciaturas y dicha beca podrá ser renovada mientras se mantenga   un promedio   mínimo de 9.0 (nueve) sin presentar materias reprobadas y el convenio siga vigente.</li>
+  			  	<li>Por cada generación del último ciclo escolar  2 (dos) becas del 25% a los alumnos con promedio mínimo 8.5 (ocho punto cinco) acumulado  en los 3  años  de  bachillerato;  esta  beca  será  valida  para  estudiar  en  el  ISU cualquiera de sus licenciaturas y dicha beca  podrá ser renovada mientras se mantenga   un promedio  mínimo de 8.5 (ocho punto cinco) sin presentar materias reprobadas y el convenio siga vigente.</li>
+  			  	<li>Pase directo a cada alumno acreedor de las becas señaladas en el inciso A) y B) por lo que no habrá necesidad de que apliquen  examen de admisión.</li>
+  			  	<li>La beca se renovará siempre y cuando:
+				Entregue oportunamente toda la documentación requerida
+				Cumpla con el reglamento interno del “ISU”
+				No cause baja</li>
+  			  </ul>
+  			  <h4>Tecnologico de Monterrey Campus Puebla</h4>
+  			  <p>www.itesm.edu/wps/wcm/connect/Campus/PUE/Puebla/</p>
+  			  <p>El reconocimiento consiste en la asignación de becas la cual se realiza con base en los resultados de la evaluación de los alumnos de 5o semestre, a quienes se les aplica un examen diagnóstico en las áreas de verbal, matemáticas y de redacción indirecta (PAA).  El puntaje máximo del examen diagnostico es de 2,400 puntos.</p>
+  			  <ul class="margin-15">
+				<li><b>Beca de Excelencia </b><br>
+				90% al mejor promedio de la generación (no transferible).</li>
+				<li><b>Becas de Talento</b> -  basadas en promedios de la generación (15 alumnos minimo)<br>
+				1950 - 2100  Promedio de Generación PAA –  una beca del 50% pura<br>
+				2101  - 2250  Promedio de Generación PAA  –  dos becas del 50% puras<br>
+				2251 - 2400 Promedio de Generación PAA  –  una beca del 80% pura</li>
+				<li>Las becas son asignadas a la generación que se gradúa.<br>
+				La beca del 80% puede ser asignada entre el 1o y 2o mejor promedio de la generación<br>
+				La beca del 50% pura puede ser transferible del 1o al 5o mejor promedio de la generación</li>
+  			  </ul>
+  			  <h4>Universidad Autónomo de Guadalajara  (UAG)</h4>
+  			  <p>www.uag.mx</p>
+  			  <ul class="margin-15">
+				<li>Con un promedio de 8.0 minimo – no hay examen de admision <br>
+					Hay que hacer los demas requisitos de tramite de admision</li>
+				<li>Con un promedio de 8.0 – 25% descuento en colegiatura</li>
+  			  </ul>
+  			  <h4>Universidad de las Americas Puebla (UDLAP)</h4>
+  			  <p>Universidad de las Americas Puebla (UDLAP)</p>
+  			  <p>Para todos aquellos alumnos con promedio acumulado de 8.0 como mínimo al término de sus estudios de preparatoria o equivalente:</p>
+  			  <ol class="margin-15">
+  			  	<li>Ingreso automático sin presentar examen de admisión y realizando tramite en el Departamento de Admisiones.</li>
+  			  	<li>Quedarán exentos del pago de la cuota del examen de admisión.</li>
+  			  	<li>Quedarán exentos del pago de la cuto única de admisión.</li>
+  			  	<li>Podrán calificar a la be “fraternitas.”  Esta beca cubrirá de manera automática y por toda la carrera al ser tramitada, un 10% de la colegiatura.  Podrá incrementarse hasta un 40% si el estudiante lo solicita, realizándose un estudio socioeconómico para la evaluación del incremento requerido.  Para obtener mayor apoyo podrá optar por un Crédito Educativo hasta de un 20%. Los estudiantes que obtengan un apoyo mayor del 10% participarán en el Programa de Formación al Estudiante Becario.  El estudiante deberá realizar el tramite en le periodo establecido por el Departamento de Beca.</li>
+  			  	<li>Los alumnos con promedio acumulado de 9.5 podrán calificar a la beca “UDLA”, obteniendo en la aplicación del examen de admisión un puntaje mínimo de 1350, que cubre del 20 a un 60% de beca por el total de la colegiatura, previo estudio socioeconómico.  El apoyo se otorga durante toda la carrera, siempre y cuando el estudiante no repruebe materias y cumpla con el reglamento de Ayuda Financiera del la UDLA Puebla.  Para obtener mayor apoyo podrá optar por un Crédito Educativo hasta de un 20%.</li>
+  			  </ol>
+  			  ',
+  );
+
+$page_es=array(
+  'metaTitle'=>'',
+  'metaDescription'=>'',
+  'title' => 'Escuelas',
+  'subtitle' => 'Eventos',
+  );
+
+if (LANG=='ES'){
+    $page=$page_es; 
+}
+else{
+    $page=$page_en;
+}
+
 echo $twig->render('middle-high-school-scholarship.html',array('main_menu'=>$main_menu,'footer'=>$footer,'sidebar'=>$sidebar,'page'=>$page));
 ?>
